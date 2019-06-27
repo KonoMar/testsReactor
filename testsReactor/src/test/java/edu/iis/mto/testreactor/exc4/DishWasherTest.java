8f97f798be49c0a95db5a91839b83309bac9fe0d
Marcin Konopka <marcinkonopka123@wp.pl>
2019-06-27 16:01:35 +0200

    add test shouldReturnTrueIfStatusIsErrorFilter

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc4/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc4/DishWasherTest.java
@@ -1,15 +1,38 @@
 package edu.iis.mto.testreactor.exc4;
 
-import static org.junit.Assert.*;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class DishWasherTest {
+    private DishWasher dishWasher;
+    private DirtFilter dirtFilter;
+    private Door door;
+    private Engine engine;
+    private RunResult runResult;
+    private WaterPump waterPump;
 
-    @Test
-    public void test() {
-        fail("Not yet implemented");
+    @Before
+    public void setUp() {
+        dirtFilter = mock(DirtFilter.class);
+        door = mock(Door.class);
+        engine = mock(Engine.class);
+        waterPump = mock(WaterPump.class);
     }
 
+    @Test
+    public void shouldReturnTrueIfStatusIsErrorFilter(){
+        //RunResult runResult = RunResult.builder().withRunMinutes(30).withStatus(Status.ERROR_FILTER).build();
+        ProgramConfiguration programConfiguration = ProgramConfiguration.builder().withProgram(WashingProgram.ECO).withTabletsUsed(true).build();
+
+        DishWasher dishWasher = new DishWasher(waterPump, engine, dirtFilter, door);
+        dishWasher.start(programConfiguration);
+        Assert.assertThat(dishWasher.start(programConfiguration).getStatus(), is(Status.ERROR_FILTER));
+    }
 }
